905a649ef864ca08ea3939509e838c2918fe9393
Regis-Ntwari <rex90danny@gmail.com>
2021-09-25 19:26:22 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HotTableModule} from '@handsontable/angular'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http'
+import { MovieListComponent } from './movie-list/movie-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HotTableModule.forRoot(),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/movie-list/movie-list.component.ts
@@ -0,0 +1,41 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import Handsontable from 'handsontable';
+
+@Component({
+  selector: 'app-movie-list',
+  templateUrl: './movie-list.component.html',
+  styleUrls: ['./movie-list.component.css']
+})
+export class MovieListComponent implements OnInit {
+
+  hotSettings : Handsontable.GridSettings = {
+    renderAllRows : false,
+    maxRows : 500,
+    colHeaders : ['ID', 'Budget', 'Genre', 'Homepage', 'Keywords'],
+    manualColumnMove : true,
+    manualColumnResize : true,
+    height : 'auto',
+    width : 'auto',
+    licenseKey : 'non-commercial-and-evaluation'
+  }
+  movie : any;
+  movies : [];
+
+  constructor(private http: HttpClient) { 
+    this.movies = []
+   }
+
+  ngOnInit(): void {
+    this.http.get("https://dry-reef-91563.herokuapp.com/api/movies")
+            .subscribe(Response => {
+              console.log(Response);
+              this.movie = Response;
+              this.movies = this.movie
+            })
+  }
+  public afterChange(e:any, hot:any){
+    hot.getHandsontableInstance().render
+  }
+
+}
